127f2751f3e0a4f897ff460249a0ee6be96a8b11
Eirik Skjærseth <eirik1502@gmail.com>
2020-09-13 15:29:22 +0200

    add magnetron logos

--- src/services/magnetronServerService/gameServerApi.ts
+++ src/services/magnetronServerService/gameServerApi.ts
@@ -1,6 +1,8 @@
 import { MagAction, MagState, MagStatePlayerView } from "./magnetronGameTypes"
 
-export const apiAddress = "https://magnetron.no" // "http://localhost:8080"
+// export const apiAddress = "https://magnetron.no"
+export const apiAddress = "http://localhost:8080"
+
 const apiPrefix = "/api"
 const apiUrl = `${apiAddress}${apiPrefix}`
 
